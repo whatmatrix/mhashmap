63d94906e5c13e5ffbc6638e594761b445382681
MinJae Hwang <minjae.hwang@gmail.com>
2013-06-22 22:57:33 -0400

    sse hashing

--- mhashmap.h
+++ mhashmap.h
@@ -14,6 +14,8 @@
 
 #include "lookup3.h"
 
+#include <xmmintrin.h>
+
 #define HASHPAGE_SIZE 128
 
 struct hash_function {
@@ -161,8 +163,8 @@ public:
 	typedef uint64_t key_t;
 	typedef uint64_t value_t;
 	static const int kMaxPlacementStatus = mhashpage::kMaxLevel + 1;
-	typedef uint32_t hash_array_t[kMaxPlacementStatus];
-
+	//typedef uint32_t hash_array_t[kMaxPlacementStatus];
+	typedef __m128i hash_array_t;
 	class iterator {
 	public:
 		iterator(mhashpage::entry_t* e) : e_(e) {}
@@ -194,6 +196,8 @@ public:
 	~mhashmap() {
 		free(page_);
 	}
+
+#define GET(k, x) ((reinterpret_cast<uint32_t*>(&k))[x])
 
 	size_t overflow_rate(int level) const {
 		size_t num_overflow = 0;
@@ -230,6 +234,11 @@ public:
 		}
 	}
 
+	void set_capacity_mask() {
+		uint32_t mask = capacity_ - 1;
+		capacity_mask_ = *reinterpret_cast<__m128i*>(&_mm_set_ps1(*reinterpret_cast<float*>(&mask)));
+	}
+
 	int load_factor() const {
 		return num_entries_ * 1000LL / mhashpage::num_max_entries / (capacity_ + num_overflow_page_);
 	}
@@ -250,9 +259,9 @@ public:
 		compute_hash(page_[i].entries[j].first, key_hash);
 
 		for (int l = 0; l < kMaxPlacementStatus; ++l) {
-			if (key_hash[l] == i) {
+			if (GET(key_hash, l) == i) {
 				for (int j = 0; j < l; ++j) {
-					++page_[key_hash[j]].cxt.foreign_placed[j];
+					++page_[GET(key_hash, j)].cxt.foreign_placed[j];
 				}
 				return true;
 			}
@@ -279,6 +288,7 @@ public:
 
 		page_ = reinterpret_cast<mhashpage*>(realloc(page_, sizeof(mhashpage) * capacity_));
 		std::memset(&page_[old_capacity], 0, sizeof(mhashpage) * (capacity_ - old_capacity));
+		set_capacity_mask();
 
 		for (int i = 0; i < old_capacity; ++i) {
 			for (int j = 0; j < mhashpage::kMaxLevel; ++j) {
@@ -303,59 +313,66 @@ public:
 		rebuild();
 	}
 
-	void compute_hash(const key_t& key, hash_array_t h) {
-		h[0] = static_cast<uint32_t>(h1_(key, 0));
-		h[1] = (key + 7438125) * 571723 % 999815601;
-		h[2] = 0;
-		h[3] = 0;
-		hashlittle2(&key, sizeof(key_t), &h[2], &h[3]);
+	void compute_hash(const key_t& key, hash_array_t& h) {
+		uint32_t k = key;
+		h = *reinterpret_cast<__m128i*>(&_mm_set_ps1(*reinterpret_cast<float*>(&k)));
 
-		if (h[0] == h[1]) {
-			++h[1];
-		}
-		if (h[1] == h[2]) {
-			++h[2];
-		}
-		if (h[2] == h[3]) {
-			++h[3];
-		}
+		h = _mm_add_epi32(h, hash_add_);
+		h = _mm_mullo_epi32(h, hash_mult_);
+		h = _mm_and_si128(h, capacity_mask_);
 
-		h[0] %= capacity_;
-		h[1] %= capacity_;
-		h[2] %= capacity_;
-		h[3] %= capacity_;
+		//h[0] = static_cast<uint32_t>(h1_(key, 0));
+		//h[1] = (key + 7438125) * 571723 % 999815601;
+		//h[2] = (key + 192387) * 1298044988 % 381782731;
+		//h[3] = (key + 47741) * 48483 % 18283747522222;
+		////hashlittle2(&key, sizeof(key_t), &h[2], &h[3]);
+
+		//if (h[0] == h[1]) {
+		//	++h[1];
+		//}
+		//if (h[1] == h[2]) {
+		//	++h[2];
+		//}
+		//if (h[2] == h[3]) {
+		//	++h[3];
+		//}
+
+		//h[0] %= capacity_;
+		//h[1] %= capacity_;
+		//h[2] %= capacity_;
+		//h[3] %= capacity_;
 
 	}
 
 
-	mhashpage::entry_t* find_internal(const key_t& k, hash_array_t key_hash) {
+	mhashpage::entry_t* find_internal(const key_t& k, hash_array_t& key_hash) {
 		mhashpage::entry_t* entry;
 		for (int i = 0; i < kMaxPlacementStatus; ++i) {
-			if ((entry = page_[key_hash[i]].find(k)) != nullptr) {
+			if ((entry = page_[GET(key_hash, i)].find(k)) != nullptr) {
 				return entry;
 			}
-			if (i != mhashpage::kMaxLevel && !page_[key_hash[i]].overflow(i)) {
+			if (i != mhashpage::kMaxLevel && !page_[GET(key_hash, i)].overflow(i)) {
 				break;
 			}
 		}
 		return nullptr;
 	}
 
-	void increase_foreign_element(int level, hash_array_t key_hash) {
+	void increase_foreign_element(int level, hash_array_t& key_hash) {
 		for (int i = 0; i < level; ++i) {
-			++page_[key_hash[i]].cxt.foreign_placed[i];
+			++page_[GET(key_hash, i)].cxt.foreign_placed[i];
 		}
 	}
 
-	void decrease_foreign_element(int level, hash_array_t key_hash) {
+	void decrease_foreign_element(int level, hash_array_t& key_hash) {
 		for (int i = 0; i < level; ++i) {
-			--page_[key_hash[i]].cxt.foreign_placed[i];
+			--page_[GET(key_hash, i)].cxt.foreign_placed[i];
 		}
 	}
 
 	bool try_insert(const mhashpage::entry_t& element, hash_array_t key_hash) {
 		for (int i = 0; i < kMaxPlacementStatus; ++i) {
-			if (page_[key_hash[i]].insert(element, i)) {
+			if (page_[GET(key_hash, i)].insert(element, i)) {
 				increase_foreign_element(i, key_hash);
 				return true;
 			}
@@ -367,7 +384,7 @@ public:
 		for (int i = 0; i < kMaxPlacementStatus; ++i) {
 			if (last_evicted_level != i) {
 				int evict_level = i;
-				if (page_[key_hash[i]].try_evict_foreign(evicted, evict_level, i)) {
+				if (page_[GET(key_hash, i)].try_evict_foreign(evicted, evict_level, i)) {
 					last_evicted_level = evict_level;
 					return true;
 				}
@@ -378,7 +395,7 @@ public:
 
 	void evict_any(mhashpage::entry_t& evicted, int& last_evicted_level, hash_array_t key_hash) {
 		int evict_level = 0;
-		bool ret = page_[key_hash[0]].try_evict_foreign(evicted, evict_level, -1);
+		bool ret = page_[GET(key_hash, 0)].try_evict_foreign(evicted, evict_level, -1);
 #ifdef _DEBUG
 		assert(ret == true);
 #endif
@@ -417,9 +434,9 @@ public:
 
 	void insert(const mhashpage::entry_t& element) {
 
-		if (load_factor() > load_factor_) {
-			rebuild_or_rehash();
-		}
+		//if (load_factor() > load_factor_) {
+		//	rebuild_or_rehash();
+		//}
 
 		hash_array_t key_hash;
 		compute_hash(element.first, key_hash);
@@ -452,7 +469,7 @@ public:
 	}
 
 private:
-	static const int MAX_ITERATION = 5;
+	static const int MAX_ITERATION = 10;
 
 	// 70% occupancy
 	static const uint32_t load_factor_ = 700;
@@ -463,13 +480,22 @@ private:
 		num_entries_ = 0;
 		num_overflow_page_ = 0;
 		std::memset(page_, 0, sizeof(mhashpage) * capacity);
+		set_capacity_mask();
+
+		static const uint32_t addv[4] = {1923775UL, 47472UL, 575757172UL, 39192381UL};
+		hash_add_ = _mm_loadu_si128(reinterpret_cast<const __m128i*>(addv)); 
+		static const uint32_t multv[4] = {512775UL, 47471093UL, 6761UL, 83192381UL};
+		hash_mult_ = _mm_loadu_si128(reinterpret_cast<const __m128i*>(multv));
 	}
 
 	mhashpage* page_;
 	int32_t num_entries_;
 	int32_t capacity_;
 	int32_t num_overflow_page_;
-	hash_function h1_;
+	//hash_function h1_;
+	hash_array_t capacity_mask_;
+	hash_array_t hash_add_;
+	hash_array_t hash_mult_;
 };
 
 #endif  // MHASHMAP_H_
